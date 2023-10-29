5d8ac9cde48278ba4d2601833ba338c5ed13addf
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-10-29 19:11:19 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ function onScroll() {
     navigation.classList.remove("scroll");
   }
 
-
 //   navigation.classList.toggle("scroll", window.scrollY > 0);
 }
+
+function openMenu(){
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
 
