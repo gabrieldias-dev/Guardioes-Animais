8a46c9cb08284f50a436ed701b3bb6d3da205793
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-10-29 22:56:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 
-// Forma que eu fiz
-function onScroll() {
-  let navigation = document.querySelector("#navigation");
 
+function onScroll() {
+  showNavOnScroll();
+  
+
+}
+
+function showNavOnScroll(){
+  let navigation = document.querySelector("#navigation");
   if (scrollY > 0) {
     navigation.classList.add("scroll");
   } else {
     navigation.classList.remove("scroll");
   }
 
-//   navigation.classList.toggle("scroll", window.scrollY > 0);
+  //   navigation.classList.toggle("scroll", window.scrollY > 0);
 }
 
 function openMenu(){
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
   document.body.classList.remove("menu-expanded");
 }
-
 
