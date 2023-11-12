56fcfd8c45774be6762150c4c7fc906167ff69d8
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-11-12 14:35:22 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -64,5 +64,19 @@ function menuAtivado(section) {
     }
 }
 
-
+// Carrossel resgate
+let swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
 
